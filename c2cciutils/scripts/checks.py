cc9ce82ae9e60db99ecc98d8fab09a4cba29d7ae
Stéphane Brunner <stephane.brunner@camptocamp.com>
2020-12-03 17:00:08 +0100

    Add --stop and --check arguments to checks

--- c2cciutils/scripts/checks.py
+++ c2cciutils/scripts/checks.py
@@ -10,6 +10,8 @@ import c2cciutils.checks
 def main() -> None:
     parser = argparse.ArgumentParser(description="Run the checks of c2cciutils.")
     parser.add_argument("--fix", action="store_true", help="fix black and isort issues")
+    parser.add_argument("--stop", action="store_true", help="stop on first error")
+    parser.add_argument("--check", help="runs only the specified check")
 
     args = parser.parse_args()
 
@@ -17,12 +19,14 @@ def main() -> None:
     config = full_config.get("checks", {})
     success = True
     for key, conf in config.items():
-        if conf:
+        if conf and (args.check is None or args.check == key):
             check = getattr(c2cciutils.checks, key)
             print("::group::Run check {}".format(key))
             if not check(conf, full_config, args):  # type: ignore
                 success = False
                 print("::endgroup::")
+                if args.stop:
+                    sys.exit(1)
                 print("With error")
             else:
                 print("::endgroup::")
