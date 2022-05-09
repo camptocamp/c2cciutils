d490a400d6b820b30c10918785c39a902175391a
Stéphane Brunner <stephane.brunner@camptocamp.com>
2022-05-09 13:34:15 +0200

    Fix publish

--- c2cciutils/publish.py
+++ c2cciutils/publish.py
@@ -269,7 +269,7 @@ def pip(
         dist = os.path.join(cwd, 'dist')
         if not os.path.exists(dist):
             os.mkdir(dist)
-        if os.path.exists("setup.py"):
+        if os.path.exists(os.path.join(cwd, "setup.py")):
             cmd = ["python3", "./setup.py", "egg_info", "--no-date"]
             cmd += (
                 ["--tag-build=dev" + datetime.datetime.now().strftime("%Y%m%d%H%M%S")]
@@ -278,8 +278,7 @@ def pip(
             )
             cmd.append("bdist_wheel")
         else:
-            cwd = os.path.join(cwd, 'dist')
-            os.mkdir(cwd)
+            os.mkdir(dist)
             cmd = ["pip", "wheel", "--no-deps", "--wheel-dir=dist", '.']
         cmd = package.get("build_command", cmd)
         subprocess.check_call(cmd, cwd=cwd, env=env)
